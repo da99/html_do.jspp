9e1ad3ace4b92471fa6ce1408834fd58184ef3da
da99 <i-hate-spam-1234567@mailinator.com>
2016-01-09 13:59:21 -0600

    Fixed: various errors.

--- main.js
+++ main.js
@@ -1,145 +1,25 @@
 "use strict";
 
-var Dum_Dum_Funcs = [];
+var Dum_Dum_Boom_Boom = function () {};
 
-function Dum_Dum_Boom_Boom_Run(args, ins, out, func) {
-  if (ins.length !== func.length)
-    throw new Error("Argument length mismatch: " + ins.length + ' !== ' + func.length);
+Dum_Dum_Boom_Boom.Designs = [];
+Dum_Dum_Boom_Boom.push = function (f) {
+  Dum_Dum_Boom_Boom.Designs.push(f);
+  return Dum_Dum_Boom_Boom;
+};
 
-  // Validate inputs:
-  var inputs_valid = _.all(args, function (arg, i) {
-    return _.all(ins[i], function (is_valid) { return is_valid(arg); });
-  });
-
-  if (!inputs_valid)
-    throw new Error("Invalid inputs: " + to_string(_.toArray(args)));
-
-  var val = func.apply(null, args);
-
-  var is_valid = _.all(out, function (f){ return f(val); });
-  if (!is_valid)
-    throw new Error("Invalid output: " + to_string(val));
-
-  return val;
-}
-
-function Dum_Dum_Boom_Boom() {
-  var dum   = this;
-  var ins   = [];
-  var out   = [];
-
-  this.exs = [];
-
-  this.example = function () {
-    dum.exs.push(_.toArray(arguments));
-    return dum;
-  }; // === func
-
-  this.in   = function () {
-    ins.push(_.toArray(arguments));
-    return dum;
-  };
-
-  this.out  = function () {
-    if (!_.all(arguments, _.isFunction))
-      throw new Error("Not all functions: " + to_string(arguments));
-
-    out = _.toArray(arguments);
-    return dum;
-  };
-
-  this.body = function (func) {
-    if (dum.exs.length === 0)
-      throw new Error('!!! No examples specified.');
-
-    if (!_.isFunction(func))
-      throw new Error('Not a function: ' + to_string(func));
-    var f = function () {
-      return Dum_Dum_Boom_Boom_Run(arguments, ins, out, func);
-    };
-
-    if (out.length === 0)
-      throw new Error('!!! No :out spec specified.');
-
-    f.design = dum;
-    Dum_Dum_Funcs = [].concat(Dum_Dum_Funcs).concat([f]);
-    return f;
-  };
-
-  return this;
-} // === func Dum_Dum_Boom_Boom
-
-var Dum_Dum_Boom_Boom_Run_Specs = function (_funcs) {
-  var do_it = _.all(arguments, function (v) {
-    return (_.isBoolean(v) && v) || (_.isFunction(v) && v());
-  });
-
-  if (!do_it)
-    return false;
-
-  _.each(Dum_Dum_Funcs, function (f) {
-    var d = f.design;
-    _.each(d.exs, function (args) {
-      log("=== spec: " + to_string(args));
-      f.apply(null, args);
-    });
-  });
-
-  log("=== PASSED");
-  return true;
+var is_array_of_functions = function (a) {
+  return _.isArray(a) && _.all(a, _.isFunction);
 }; // === func
 
-
-// === Helpers ===================================================================
-
-
-var log = function (_args) {
-  if (is_localhost)
-    return console.log.apply(console, arguments);
-
-  return this;
-}; // === func
-
-
-var length_of = (new Dum_Dum_Boom_Boom())
-.example(4)
-.in(_.isFinite)
-.out(_.isFunction)
-.body(function (num) {
-  return function (v) { return v.length === num; };
-});
-
-var length_gt = (new Dum_Dum_Boom_Boom())
-.base(length_of)
-.body(function (num) {
-  return function (v) { return v.length > num;};
-});
-
-var is_string = (new Dum_Dum_Boom_Boom())
-.example("v")
-.in(_.isString)
-.out(_.isBoolean)
-.body(
-  function (v)   { return typeof v === "string"; }
-);
-
-
-var all = (new Dum_Dum_Boom_Boom())
-.example(_.isString)
-.in(_.isFunction)
-.out(_.isFunction)
-.body(
-  function (f) {
-    return (new Dum_Dum_Boom_Boom())
-    .example([])
-    .in(_.isArray)
-    .out(_.isBoolean)
-    .body(function (arr) {
-      return _.all(arr, f);
-    });
-  }
-);
-
+var is_function = function (f) { return typeof f === 'function'; };
+var is_zero     = function (v) { return v === 0; };
+var is_pos_int  = function (v) { return _.isFinite(v) && v > 0; }; // === func
+var is_$ = function (v) {
+  return v &&
+    typeof v.html === 'function' &&
+      typeof v.attr === 'function';
+};
 var to_string = function (val) {
   if (val === null)
     return "null";
@@ -153,11 +33,175 @@ var to_string = function (val) {
   if (_.isString(val))
     return '"' + val + '"';
 
-  if (_.isFinite(val.length) && val.callee)
+  if (is(val.length, or(is_zero, is_pos_int)) && val.callee)
     return to_string(_.toArray(val));
 
   return val.toString();
 }; // === func
+var to_arg = function (val) { return function (f) { return f(val); }; };
+var i = function (_args) { return {type: "input",   args: arguments}; };
+var o = function (_args) { return {type: "output",  args: arguments}; };
+var e = function (_args) { return {type: "example", args: arguments}; };
+
+var f = function () {
+  var design = {
+    example : [],
+    input   : [],
+    output  : [],
+    f       : null,
+    fin     : null
+  };
+
+  var arg_l = arguments.length;
+  var i     = 0;
+  var a     = null;
+  while (i < arg_l) {
+    a = arguments[i];
+
+    switch (typeof a) {
+      case "function":
+        design.f = a;
+        break;
+
+      default:
+        switch (a.type) {
+          case "output":
+            design[a.type] = [].concat(design[a.type]).concat(_.toArray(a.args));
+          break;
+
+          default:
+            design[a.type].push(_.toArray(a.args));
+        } // === switch a.type
+    } // === switch typeof a
+    i = i + 1;
+  } // === while
+
+
+  // ===============
+  if (_.isEmpty(design.example))
+    throw new Error('!!! No examples specified.');
+
+  if (_.isEmpty(design.output))
+    throw new Error('!!! No :out spec specified.');
+
+  if (!_.all(design.input, is_array_of_functions))
+    throw new Error('!!! Not a function: ' + to_string(design.input));
+
+  if (!is_array_of_functions(design.output))
+    throw new Error('!!! Not an array of functions: ' + to_string(design.output));
+
+  if (!_.isFunction(design.f))
+    throw new Error('Not a function: ' + to_string(design.f));
+
+  design.fin = function () {
+    var i = design.input;
+    var f = design.f;
+    var o = design.output;
+
+    if (i.length !== f.length)
+      throw new Error("Argument length mismatch: " + i.length + ' !== ' + f.length);
+
+    // Validate inputs:
+    _.all(arguments, function (arg, i) {
+      if (!_.all(i[i], to_arg(arg)))
+        throw new Error("Invalid inputs: " + to_string(arg) + " for: " + to_string(f));
+    });
+
+    var val = f.apply(null, arguments);
+
+    // Validate output:
+    if (!_.all(o, to_arg(val)))
+      throw new Error("Invalid output: " + to_string(val));
+
+    return val;
+  };
+
+  Dum_Dum_Boom_Boom.push(design);
+
+  return design.fin;
+}; // === func f
+
+var run_specs = function (_funcs) {
+  var do_it = _.all(arguments, function (v) {
+    return (_.isBoolean(v) && v) || (_.isFunction(v) && v());
+  });
+
+  if (!do_it)
+    return false;
+
+  _.each(Dum_Dum_Boom_Boom.Designs, function (d) {
+    _.each(d.exs, function (args) {
+      log("=== spec: " + to_string(args));
+      d.f.apply(null, args);
+    });
+  });
+
+  log("=== PASSED");
+  return true;
+}; // === func
+
+
+// === Helpers ===================================================================
+
+var or = function () {
+  var funcs = arguments;
+  return function (arg) {
+    return _.any(funcs, function (f) { return f(arg); });
+  };
+};
+
+var is = function (x, _funcs) {
+  var args  = _.toArray(arguments);
+  x         = args.shift();
+  var funcs = args;
+  if (_.isEmpty(funcs))
+    throw new Error("No functions specified.");
+  return _.all(funcs, to_arg(x));
+};
+
+var log = function (_args) {
+  if (is_localhost)
+    return console.log.apply(console, arguments);
+
+  return this;
+}; // === func
+
+
+var length_of = f(
+  e(4),
+  i(or(is_zero, is_pos_int)),
+  o(is_function),
+  function (num) {
+    return function (v) { return v.length === num; };
+  }
+);
+
+var length_gt = f(
+  e(0),
+  i(or(is_zero, is_pos_int)),
+  o(is_function),
+  function (num) {
+    return function (v) { return v.length > num;};
+  }
+);
+
+var is_string = _.isString;
+var is_array  = _.isArray;
+var is_bool   = _.isBoolean;
+
+
+var all = f(
+  e(is_string),
+  i(_.isFunction),
+  o(_.isFunction),
+  function (f) {
+    return f(
+      e([]), i(is_array), o(is_bool),
+      function (arr) { return _.all(arr, f); }
+    );
+  }
+);
+
 
 var is_empty = function (v) {
   var l = v.length;
@@ -177,23 +221,11 @@ var is_localhost = function () {
 
 var anything = function () { return true; };
 
-var or = function () {
-  var funcs = arguments;
-  return function (arg) {
-    return _.any(funcs, function (f) { return f(arg); });
-  };
-};
-
 var all_funcs = function (arr) {
   var l = arr.length;
   return _.isFinite(l) && l > 0 && _.all(arr, _.isFunction);
 };
 
-var is_$ = function (v) {
-  return v &&
-    typeof v.html === 'function' &&
-      typeof v.attr === 'function';
-};
 
 var l            = function (v) {
   if (!_.isFinite(v.length))
@@ -202,28 +234,26 @@ var l            = function (v) {
 };
 
 
-var dom = (new Dum_Dum_Boom_Boom())
-.example("p")
-.example("body")
-.example(jQuery("body"))
-.in(or(is_string, is_$))
-.out(is_$)
-.body(function (s) { return jQuery(s); })
-;
+var dom = f(
+  e("p"), e("body"), e(jQuery("body")),
+  i(or(is_string, is_$)),
+  o(is_$),
+  function (s) { return jQuery(s); }
+);
 
-var non_empty_$ = (new Dum_Dum_Boom_Boom())
-.example("body")
-.in(anything)
-.out(length_gt(0))
-.body(function (v) {
-  return dom(v);
-})
-;
+var non_empty_$ = f(
+  e("body"),
+  i(anything),
+  o(length_gt(0)),
+  function (v) {
+    return dom(v);
+  }
+);
 
 log(l( dom('p') ));
 log(l( dom($("p")) ));
 
-Dum_Dum_Boom_Boom_Run_Specs(is_localhost);
+run_specs(is_localhost);
 log("THE_FILE_DATE");
 
 
