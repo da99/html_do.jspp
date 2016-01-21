ce17fb82f2b631e66643a5ebeb6b03eae51238c3
da99 <i-hate-spam-1234567@mailinator.com>
2016-01-21 15:24:07 -0600

    Imported: specs from applet.js repo

--- main.js
+++ main.js
@@ -654,6 +654,22 @@ function keys_or_indexes(v) {
   return a;
 }
 
+// it 'returns an Array when passed a String'
+spec(node_array, ['<div id="111" show_if="happy?"><span></span></div>'], [
+  {
+    tag:   'DIV',
+    attrs:  {id: '111', show_if: 'happy?'},
+    custom: {},
+    childs: [
+      {tag: 'SPAN', attrs: {}, custom: {}, childs: []}
+    ]
+  }
+]);
+
+returns(['a', undefined, 'b'], function node_array_returns_raw_text_nodes() {
+  var arr = node_array('<div><span>a<span></span>b</span></div>');
+  return _.pluck(arr[0].childs[0].childs, 'nodeValue');
+});
 function node_array(unknown) {
   var arr = [];
   _.each($(unknown), function (dom) {
