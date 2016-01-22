59c7680b57bb7484331001e95ad8cc21bdf43aa6
da99 <i-hate-spam-1234567@mailinator.com>
2016-01-21 23:06:18 -0600

    Changed: get_property -> own_property

--- main.js
+++ main.js
@@ -18,17 +18,17 @@ function dot(name) {
 } // === func dot
 
 
-returns(3, function get_property_returns_own_property() {
-  return get_property('num')({num: 3});
+returns(3, function own_property_returns_own_property() {
+  return own_property('num')({num: 3});
 });
-throws(get_property('num'), [{n:4}], 'Key not found: "num" in {"n":4}');
-function get_property(name) {
-  return function _get_property_(o) {
+throws(own_property('num'), [{n:4}], 'Key not found: "num" in {"n":4}');
+function own_property(name) {
+  return function _own_property_(o) {
     if (!o.hasOwnProperty(name))
       throw new Error('Key not found: ' + to_string(name) + ' in ' + to_string(o));
     return o[name];
   };
-} // === func get_property
+} // === func own_property
 
 spec(is_localhost, [], window.location.href.indexOf('/dum_dum_boom_boom/example.html') > 0);
 function is_localhost() {
