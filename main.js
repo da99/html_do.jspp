f114dfc545f1b6a9299fdf96324b5f631e768bc7
da99 <i-hate-spam-1234567@mailinator.com>
2016-01-24 00:49:11 -0600

    Dev. checkpoint.

--- main.js
+++ main.js
@@ -1053,14 +1053,14 @@ function eachs() {
 }
 
 function map_x(coll, f) {
-  should_be(f, is_function);
   should_be(coll, is_enumerable);
+  should_be(f, is_function);
   return _.map(coll, function (x) { return f(x); });
 }
 
 function each_x(coll, f) {
-  should_be(f, is_function);
   should_be(coll, is_enumerable);
+  should_be(f, is_function);
   return eachs(coll, function (_i, x) {
     return f(x);
   });
