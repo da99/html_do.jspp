0e0808f42274116c78dd0c98f7e8b1efb0271c5e
da99 <i-hate-spam-1234567@mailinator.com>
2016-02-13 04:17:32 -0600

    Added: form-to-obj bower dep.

--- main/main.js
+++ main/main.js
@@ -1640,18 +1640,18 @@ function template(msg) {
 } // ==== funcs: template ==========
 
 function on_click(msg) {
-  if (!msg_match({dom_id: is_string}, msg))
-    return;
-  if (!msg.args || !is_function_name(msg.args[0]))
+  if (!msg_match({dom_id: is_string, args: and(is_array, has_length(1))}, msg))
     return;
 
   var dom_id = msg.dom_id;
-  var func = window[msg.args[0]];
+  var func = should_be(window[msg.args[0]], is_function);
 
   if (!on_click.processed)
     on_click.processed = {};
+
   if (on_click.processed[dom_id])
     throw new Error('#' + dom_id + ' already processed by on_click');
+
   on_click.processed[dom_id] = true;
 
   $('#' + msg.dom_id).on("click", function (e) {
