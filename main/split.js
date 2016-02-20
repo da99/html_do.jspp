7102d4087cd8da84223c634bddce7c10bacae0c0
da99 <i-hate-spam-1234567@mailinator.com>
2016-02-20 00:59:01 -0600

    Fixed: leading/trailing whitespace during file comparison

--- main/split.js
+++ main/split.js
@@ -8,14 +8,37 @@ const fs      = require('fs');
 const path    = require('path');
 const he      = require('he');
 
-var files = _.uniq(_.compact(_.map(process.argv, function (file) {
-  if (file.indexOf('.html') > 0)
-    return file;
-})));
+var args = process.argv.slice(2);
+var file = args.shift();
+var dir  = args.shift() || path.dirname(file);
 
-log(files);
+var contents = fs.readFileSync(file).toString();
+var $ = cheerio.load(contents);
+
+section_to_file('script', $, dir + '/script.js');
+section_to_file('head', $, dir + '/head.html');
+section_to_file('style', $, dir + '/style.css');
+fs.writeFileSync(dir + '/markup.html', $.html());
 
 
 function log(_args) {
   return console.log.apply(console, arguments);
+}
+
+function section_to_file(tag, $, file_path) {
+  var targets = $(tag);
+
+  if (targets.length === 0)
+    return false;
+
+  if (targets.length > 1) {
+    console.error("!!! Too many tags for: " + tag);
+    process.exit(1);
+  }
+
+  var target = targets[0];
+  var contents = $(target).html();
+  $(target).remove();
+  fs.writeFileSync(file_path, contents);
+  return true;
 }
