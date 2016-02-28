143accd9ca69c35cf62822a7b2dee5e06126eedf
da99 <i-hate-spam-1234567@mailinator.com>
2016-02-28 16:17:14 -0600

    Fixes: for testing and building

--- build/node.js
+++ build/node.js
@@ -16,11 +16,13 @@ function spec_new(str_or_func) {
     "use strict";
     if (!is_spec_env()) return false;
     // === Is there a specific spec to run?
-    var href = window.location.href;
-    var target = _.trim(href.split("?").pop() || "");
-    if (!is_empty(target) && target !== href && target !== function_to_name(str_or_func)) return false;
-    // === Reset DOM:
-    spec_dom("reset");
+    if (typeof window !== "undefined") {
+        var href = window.location.href;
+        var target = _.trim(href.split("?").pop() || "");
+        if (!is_empty(target) && target !== href && target !== function_to_name(str_or_func)) return false;
+        // === Reset DOM:
+        spec_dom("reset");
+    }
     return true;
 }
 
