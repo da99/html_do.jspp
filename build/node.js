253caee500d855971fd4445c580d3518b4ed8849
da99 <i-hate-spam-1234567@mailinator.com>
2016-02-28 05:27:52 -0600

    Fixed: typos

--- build/node.js
+++ build/node.js
@@ -1548,6 +1548,82 @@ function find(_funcs) {
     };
 }
 
+/* jshint strict: true, undef: true */
+/* globals Computer, is_empty */
+// === Examples:
+// App()
+// App(..args for underlying Computer)
+function App() {
+    "use strict";
+    if (!App._computer) {
+        App._computer = new Computer();
+    }
+    if (!is_empty(arguments)) App._computer.apply(null, arguments);
+    return App;
+}
+
+/* jshint strict: true, undef: true */
+/* globals is_array, spec_returns, arguments_are, reduce_eachs, copy_value, do_it, and, is, is_plain_object */
+/* globals to_string, apply_function, has_length, is_function, msg_match, function_to_name */
+spec_returns(3, function() {
+    "use strict";
+    var counter = 0;
+    var data = {
+        my_name: "happy"
+    };
+    var state = new Computer();
+    state("push", function(msg) {
+        if (!msg_match({
+            my_name: "happy"
+        }, msg)) return;
+        ++counter;
+    });
+    do_it(3, function() {
+        state("run", data);
+    });
+    return counter;
+});
+
+function Computer() {
+    "use strict";
+    return State;
+    function State(action, args) {
+        if (action === "invalid") {
+            State.is_invalid = true;
+            return;
+        }
+        if (State.is_invalid === true) throw new Error("state is invalid.");
+        if (!is_array(State.funcs)) State.funcs = [];
+        var funcs = State.funcs.slice(0);
+        switch (action) {
+          case "push":
+            arguments_are(arguments, is("push"), and(is_function, has_length(1)));
+            var func = arguments[1];
+            if (func.length !== 1) throw new Error("function.length needs to === 1: " + function_to_name(func));
+            State.funcs = funcs.slice(0).concat([ func ]);
+            return true;
+
+          case "run":
+            arguments_are(arguments, is("run"), is_plain_object);
+            var msg = arguments[1];
+            return reduce_eachs([], funcs, function(acc, _ky, func) {
+                try {
+                    var msg_copy = copy_value(msg);
+                    acc.push(apply_function(func, [ msg_copy ]));
+                } catch (e) {
+                    State("invalid");
+                    throw e;
+                }
+                return acc;
+            });
+
+          default:
+            State("invalid");
+            throw new Error("Unknown action for state: " + to_string(action));
+        }
+    }
+}
+
 /* jshint esnext: true, strict: true, undef: true */
 /* globals log */
 function log_and_return(v) {
@@ -1577,4 +1653,4 @@ function is_dir(v) {
         return false;
     }
 }
-//# sourceMappingURL=build/node.js.map
+//# sourceMappingURL=build/browser.js.map
