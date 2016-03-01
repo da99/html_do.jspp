055aa5b0fd30e52ab23eb61f31e892fa8e7c8953
da99 <i-hate-spam-1234567@mailinator.com>
2016-03-01 07:10:44 -0600

    Added: prepend-exports.sh

--- bin/lib/build.sh
+++ bin/lib/build.sh
@@ -2,36 +2,39 @@
 # === {{CMD}}
 build () {
 
-    ugly () {
-      local name="$1"; shift
-      local output="build/${name}"
-      local file="$output.js"
+  ugly () {
+    local name="$1"; shift
+    local output="build/${name}"
+    local file="$output.js"
 
-      rm -f "$file"
-      rm -f "$file".map
-      names="$@"
-      uglifyjs                           \
-        -b --comments all                 \
-        --screw-ie8                        \
-        $($0 top-build-files    $names)     \
-        $($0 body-build-files   $names)      \
-        $($0 bottom-build-files $names)       \
-        --source-map "$file".map > "$file"
+    rm -f "$file"
+    rm -f "$file".map
+    names="$@"
+    uglifyjs                           \
+      -b --comments all                 \
+      --screw-ie8                        \
+      $($0 top-build-files    $names)     \
+      $($0 body-build-files   $names)      \
+      $($0 bottom-build-files $names)       \
+      --source-map "$file".map > "$file"
 
-      dups="$($0 print-dups "$file" || :)"
-      if [[ ! -z "$dups" ]]; then
-        echo -e "!!! ${Red}Dups found${Color_Off}:\n$dups" 1>&2
-        exit 1
-      fi
+    dups="$($0 print-dups "$file" || :)"
+    if [[ ! -z "$dups" ]]; then
+      echo -e "!!! ${Red}Dups found${Color_Off}:\n$dups" 1>&2
+      exit 1
+    fi
 
-      js_setup jshint "$file"
-    }
+    js_setup jshint "$file"
+  }
 
-    echo -e "=== Building: ${Bold}node.js${Color_Off}"
-    ugly "node"     spec base state node
-    node "build/node.js" test
-    echo -e "=== Done building: ${Bold}node.js${Color_Off}"
+  # === NODE: =================================================================
+  echo -e "=== Building: ${Bold}node.js${Color_Off}"
+  ugly "node" spec base state node
+  node "lib/node.spec.js" test
+  echo -e "=== Done building: ${Green}node.js${Color_Off}"
 
-    ugly "browser"  spec base state dom   data-do
-    echo -e "=== ${Bold}Test for browser.js not ready yet${Color_Off}"
+  # === BROWSER: ==============================================================
+  ugly "browser"  spec base state dom   data-do
+  echo -e "=== Test for ${Orange}browser.js${Color_Off} not ready yet"
+
 } # === end function
---
+++ bin/lib/prepend-exports.sh
@@ -0,0 +1,15 @@
+
+prepend-exports () {
+  for FILE in lib/base/*.js lib/node/*.js lib/state/*.js lib/spec/*.js; do
+    [[ "$FILE" == */_.*.js ]] && continue || :
+    funcs_line_nums="$(grep -Po --line-number '^function\s+\K(.+?)(?=\()' "$FILE")"
+    [[ -z "$funcs_line_nums" ]] && continue || :
+    grep 'export(' "$FILE" && continue || :
+    grep 'private ' "$FILE" && { echo "=== Skipping private: $FILE"; continue; } || :
+    while IFS=':' read LINE NAME; do
+      new_content="export($NAME);"
+      echo -e "=== ${Bold}ADDING${Color_Off}: $new_content -> $FILE"
+      echo sed -i "${LINE}i $new_content" "$FILE"
+    done < <(echo -e "$funcs_line_nums")
+  done
+}
