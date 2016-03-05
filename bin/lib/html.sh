d49177a6eaed8d7fbee44a1df422902939049e45
da99 <i-hate-spam-1234567@mailinator.com>
2016-03-05 00:59:24 -0600

    Changed: html files specified for bin

---
+++ bin/lib/html.sh
@@ -0,0 +1,13 @@
+
+# === {{CMD}} dum-dum.file.html
+html () {
+  node $THIS_DIR/lib/html.js $@
+  # TODO: remove duplicate nodes: link, src, meta
+  # vnu="$(echo tmp/validator/*/dist/vnu.jar)"
+  # final="$(node $THIS_DIR/lib/html.js $THE_ARGS)"
+  # # === If <body is found, validate whole document:
+  # if echo "$final" | grep -i '<body' &>/dev/null; then
+  #   echo "$final" | java -jar "$vnu" - || { stat="$?"; echo $action 1>&2; exit $stat; }
+  # fi
+  # echo "$final"
+} # === end function
