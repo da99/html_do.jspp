d1aeaa4d4f59fdaa8a85e2a9d08aa74496a55302
da99 <i-hate-spam-1234567@mailinator.com>
2016-04-15 17:22:27 -0500

    Fixed: various. Touchups: bump-commit

--- bin/lib/bump-commit.sh
+++ bin/lib/bump-commit.sh
@@ -2,12 +2,25 @@
 # === {{CMD}}  major|minor|patch
 # === runs `js_setup upgrade`, then `{{BIN}} build`, then runs `js_setup bump-commit $@`
 bump-commit () {
+
+  local +x NEW_VERSION="$(git_setup bump $@)"
+  local +x NEW_NUM=${NEW_VERSION/v/}
+
+  mksh_setup BOLD "=== {{Bumping}} to $NEW_VERSION"
+
   mksh_setup BOLD "=== Upgrading..."
+  js_setup js_clean
   js_setup upgrade
+  git_setup should-be-clean
 
   mksh_setup BOLD "=== Building and testing..."
   $0 build
 
-  mksh_setup BOLD "=== {{Bumping}} $@"
-  js_setup bump-commit $@
+  js_setup update-key "package.json" "version" "$NEW_NUM"
+  git add package.json
+  git commit -m "Bump: $NEW_VERSION"
+
+  git tag "$NEW_VERSION"
+  git push origin "$NEW_VERSION"
+  git push
 } # === end function
